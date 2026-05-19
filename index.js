beec9b4f22940df345c4a918da228dff3b1101b4
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2026-05-19 22:38:13 +0530

    Feat: more on addeventlistner

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let h1 = document.querySelector('h1')
-h1.addEventListener('click', function(){
-h1.style.color = "green"
+let h1 = document.querySelector("h1")
+
+window.addEventListener("keydown", function (dets) {
+    h1.textContent = dets.key;
 })
