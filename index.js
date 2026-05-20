e739dcf4b27b8436f4ec095e103b7dd0eef7d5a3
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2026-05-20 22:36:51 +0530

    feat: profile design implemented

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-let btn = document.querySelector("#btn");
-let fileinp = document.querySelector("#fileinp");
+// let btn = document.querySelector("#btn");
+// let fileinp = document.querySelector("#fileinp");
 
-btn.addEventListener("click", function () {
-  fileinp.click();
-});
+// btn.addEventListener("click", function () {
+//   fileinp.click();
+// });
 
-fileinp.addEventListener("change", function (dets) {
- const file = dets.target.files[0];
- if(file){
-    btn.textContent = file.name;
- }
+// fileinp.addEventListener("change", function (dets) {
+//  const file = dets.target.files[0];
+//  if(file){
+//     btn.textContent = file.name;
+//  }
+// });
+
+let form = document.querySelector("form");
+let inputs = document.querySelectorAll("input");
+
+form.addEventListener("submit", function (dets) {
+  dets.preventDefault();
+
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  let profile = document.createElement("div");
+  profile.classList.add("profile");
 });
