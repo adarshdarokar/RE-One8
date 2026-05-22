9b40458cc18b3fde458b28379a68a4b9b7cbddec
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2026-05-22 17:54:47 +0530

    feat: event bubbling added

--- index.js
+++ index.js
@@ -1,45 +1,14 @@
-// let form = document.querySelector("form");
-// let inputs = document.querySelectorAll("input");
-// let main = document.querySelector("#main");
 
-// form.addEventListener("submit", function (dets) {
-//   dets.preventDefault();
+// let tango = document.querySelector("#tango");
+// window.addEventListener("mousemove", function (dets) {
+//   tango.style.top = dets.clientY + "px";
+//   tango.style.left = dets.clientX + "px";
 
-//   let card = document.createElement("div");
-//   card.classList.add("card");
+// })
 
-//   let profile = document.createElement("div");
-//   profile.classList.add("profile");
+let ul = document.querySelector("ul")
 
-//   let img = document.createElement("img");
-//   img.setAttribute("src", inputs[0].value);
 
-//   let h3 = document.createElement("h3");
-//   h3.textContent = inputs[1].value;
-
-//   let h5 = document.createElement("h5");
-//   h5.textContent = inputs[2].value;
-
-//   let p = document.createElement("p");
-//   p.textContent = inputs[3].value;
-
-//   profile.appendChild(img);
-//   card.appendChild(profile);
-
-//   card.appendChild(h3);
-//   card.appendChild(h5);
-//   card.appendChild(p);
-
-//   main.appendChild(card);
-// });
-
-// form for user profile //
-
-let tango = document.querySelector("#tango");
-
-tango.addEventListener("mouseover", function () {
-  tango.style.backgroundColor = "blue";
-});
-tango.addEventListener("mouseout", function () {
-  tango.style.backgroundColor = "black";
-});
+ul.addEventListener("click",function(dets){
+  dets.target.classList.toggle("lt")
+})
