9962aa5f93d9fd6d724cf007ee266188a2e044f7
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2026-05-22 12:41:04 +0530

    feat: mouseover,mouseout commit

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-// let btn = document.querySelector("#btn");
-// let fileinp = document.querySelector("#fileinp");
+// let form = document.querySelector("form");
+// let inputs = document.querySelectorAll("input");
+// let main = document.querySelector("#main");
 
-// btn.addEventListener("click", function () {
-//   fileinp.click();
+// form.addEventListener("submit", function (dets) {
+//   dets.preventDefault();
+
+//   let card = document.createElement("div");
+//   card.classList.add("card");
+
+//   let profile = document.createElement("div");
+//   profile.classList.add("profile");
+
+//   let img = document.createElement("img");
+//   img.setAttribute("src", inputs[0].value);
+
+//   let h3 = document.createElement("h3");
+//   h3.textContent = inputs[1].value;
+
+//   let h5 = document.createElement("h5");
+//   h5.textContent = inputs[2].value;
+
+//   let p = document.createElement("p");
+//   p.textContent = inputs[3].value;
+
+//   profile.appendChild(img);
+//   card.appendChild(profile);
+
+//   card.appendChild(h3);
+//   card.appendChild(h5);
+//   card.appendChild(p);
+
+//   main.appendChild(card);
 // });
 
-// fileinp.addEventListener("change", function (dets) {
-//  const file = dets.target.files[0];
-//  if(file){
-//     btn.textContent = file.name;
-//  }
-// });
+// form for user profile //
 
-let form = document.querySelector("form");
-let inputs = document.querySelectorAll("input");
+let tango = document.querySelector("#tango");
 
-form.addEventListener("submit", function (dets) {
-  dets.preventDefault();
-
-  let card = document.createElement("div");
-  card.classList.add("card");
-
-  let profile = document.createElement("div");
-  profile.classList.add("profile");
+tango.addEventListener("mouseover", function () {
+  tango.style.backgroundColor = "blue";
+});
+tango.addEventListener("mouseout", function () {
+  tango.style.backgroundColor = "black";
 });
