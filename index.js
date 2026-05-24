da5a79011ad0847b8cf1e0ae56d49d01e4505098
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2026-05-24 11:24:37 +0530

    feat: different types of interval's

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
+// setTimeout(function(){
+//   console.log("hollo");
+  
+// }, 5000);
 
-// let tango = document.querySelector("#tango");
-// window.addEventListener("mousemove", function (dets) {
-//   tango.style.top = dets.clientY + "px";
-//   tango.style.left = dets.clientX + "px";
-
-// })
-
-// let ul = document.querySelector("ul")
+// ek bar chalta ha //
 
 
-// ul.addEventListener("click",function(dets){
-//   dets.target.classList.toggle("lt")
-// })
+
+// setInterval(function(){
+// console.log("hello lome");
+
+// },4000)
+
+// baar baar chalta ha //
 
 
-let nm = document.querySelector("#name");
-let form = document.querySelector("form");
 
-form.addEventListener("submit", function (dets) {
-  dets.preventDefault();
 
-  if (nm.value.length <= 2) {
-    document.querySelector("#hide").style.display = "initial";
-  } else {
-    document.querySelector("#hide").style.display = "none";
-  }
-});
+// let chalo = 10;
+// let intro = setInterval(function(){
+//   if(chalo >=1){
+//     chalo--;
+//     console.log(chalo);
+    
+//   } else
+//     clearInterval(intro)
+// },1000)
+
+//counting 9-0 //
+
+
+
